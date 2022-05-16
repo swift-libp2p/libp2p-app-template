d8babbc63b677c9b1386bfe6facff3ac800e91b9
Brandon <btoms.20@gmail.com>
2022-05-16 13:05:01 -0700

    Updated default config code

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,18 +5,20 @@ import LibP2PMPLEX
 // configures your application
 public func configure(_ app: Application) throws {
     
-    /// We can specify the global log level here
+    // We can specify the global log level here
     app.logger.logLevel = .notice
 
-    /// Configure your networking stack...
+    // Configure your networking stack...
     app.security.use(.noise)
     app.muxers.use(.mplex)
     
-    /// Lets start a TCP server on the localhost bound to port 10000
+    // Lets start a TCP server on the localhost bound to port 10000
     app.listen(.tcp(host: "127.0.0.1", port: 10000))
     
     // register routes
     try routes(app)
     
-    app.logger.notice("Server listening at \(app.listenAddresses.first!)/p2p/\(app.peerID.b58String).")
+    app.eventLoopGroup.next().scheduleTask(in: .milliseconds(100)) {
+        app.logger.notice("Libp2p listening at \(app.listenAddresses.first!)/p2p/\(app.peerID.b58String).")
+    }
 }
