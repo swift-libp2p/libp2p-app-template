1d565abcc21dc7b1ab7d48d27f9ed6aaff98bacf
Brandon <32753167+btoms20@users.noreply.github.com>
2026-01-16 17:20:07 -0800

    bumped swift-libp2p to 3.3

--- Package.swift
+++ Package.swift
@@ -10,8 +10,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/swift-libp2p/swift-libp2p", .upToNextMinor(from: "0.3.0")),
+        .package(url: "https://github.com/swift-libp2p/swift-libp2p", .upToNextMinor(from: "0.3.3")),
         // Noise Security Module
         .package(url: "https://github.com/swift-libp2p/swift-libp2p-noise", .upToNextMinor(from: "0.2.0")),
         // YAMUX Muxer Module
