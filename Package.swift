cf9c2df77c513070c4f7cc2e1e213abcb9379059
Brandon <32753167+btoms20@users.noreply.github.com>
2026-01-19 13:00:16 -0800

    template injectors

--- Package.swift
+++ Package.swift
@@ -4,17 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "libp2p-app-template",
+    name: "%%APP_NAME%%",
     platforms: [
         .macOS(.v13)
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/swift-libp2p/swift-libp2p", .upToNextMinor(from: "0.3.3")),
-        // Noise Security Module
-        .package(url: "https://github.com/swift-libp2p/swift-libp2p-noise", .upToNextMinor(from: "0.2.0")),
-        // YAMUX Muxer Module
-        .package(url: "https://github.com/swift-libp2p/swift-libp2p-yamux", .upToNextMinor(from: "0.2.0")),
+        %%DEPENDENCY%%
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,8 +20,7 @@ let package = Package(
             name: "App",
             dependencies: [
                 .product(name: "LibP2P", package: "swift-libp2p"),
-                .product(name: "LibP2PNoise", package: "swift-libp2p-noise"),
-                .product(name: "LibP2PYAMUX", package: "swift-libp2p-yamux"),
+                %%TARGET_DEPENDENCY%%
             ],
             swiftSettings: swiftSettings),
         .testTarget(
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,6 +1,4 @@
-import LibP2P
-import LibP2PNoise
-import LibP2PYAMUX
+%%IMPORT%%
 
 // configures your application
 public func configure(_ app: Application) async throws {
@@ -8,12 +6,11 @@ public func configure(_ app: Application) async throws {
     // We can specify the global log level here
     app.logger.logLevel = .notice
 
-    // Configure your networking stack...
-    app.security.use(.noise)
-    app.muxers.use(.yamux)
+    // Install our modules on libp2p
+    %%INSTALLATION%%
     
-    // Lets start a TCP server on the localhost bound to port 10000
-    app.listen(.tcp(host: "127.0.0.1", port: 10000))
+    // Configure the modules to be used
+    %%POST_INSTALLATION%%
     
     // Add a custom command
     app.asyncCommands.use(Cowsay(), as: "cowsay")
